658e2581aa8169831f1fbf493fc03a8b3f463ee5
Rafael <rafaelribeirosouza86@gmail.com>
2024-08-30 14:14:17 -0300

    updating test

---
+++ test/string/string_test.go
@@ -0,0 +1,43 @@
+package slice
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	Connection "github.com/rafaelsouzaribeiro/go-socket/internal/infra/web"
+	"github.com/rafaelsouzaribeiro/go-socket/pkg/factory"
+	serverstring "github.com/rafaelsouzaribeiro/go-socket/test/server-string"
+)
+
+var Once sync.Once
+
+func BenchmarkClient(b *testing.B) {
+	Once.Do(func() {
+		go serverstring.RunServer()
+		time.Sleep(1 * time.Second)
+	})
+
+	for i := 0; i < b.N; i++ {
+		connect := Connection.New("localhost", "8585")
+		conn, err := connect.ConnectionClient()
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer conn.Close()
+
+		factories := factory.NewClient(Connection.Person{}, nil, "Message")
+		buffer, err := factories.GetClient()
+
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = conn.Write(buffer)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
